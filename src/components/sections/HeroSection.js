91729e8ed9eb449feb973bb61ca5a8c5b343eeb2
Dung Phan <madewithcrayons@gmail.com>
2021-03-21 16:17:27 +0100

    styling: component and global styles

--- src/components/sections/HeroSection.js
+++ src/components/sections/HeroSection.js
@@ -1,6 +1,7 @@
 import React from "react"
-// import { Link } from "gatsby"
 import styled from "styled-components"
+import { themes } from "../styles/ColorStyles"
+import { H1, MediumText } from "../styles/TextStyles"
 function HeroSection() {
   return (
     <Wrapper>
@@ -34,12 +35,7 @@ const TextWrapper = styled.div`
   display: grid;
   gap: 30px;
 `
-const Title = styled.div`
-  font-weight: bold;
-  font-size: 60px;
-  color: white;
+const Title = styled(H1)`
+  color: ${themes.dark.text1};
 `
-const Description = styled.div`
-  font-size: 17px;
-  line-height: 130%;
-`
+const Description = styled(MediumText)``
