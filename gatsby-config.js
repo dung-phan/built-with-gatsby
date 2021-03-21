12a5839c11ca93629f46415895aaebf1790ff196
Dung Phan <madewithcrayons@gmail.com>
2021-03-21 15:28:31 +0100

    styling: use styled components & section structure

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
+    title: `Portfolio with Gatsby`,
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    author: `@_DungPhan`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
