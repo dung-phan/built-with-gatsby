30acc0563bf95f01b70275ff01e3b05f405187ec
Dung Phan <madewithcrayons@gmail.com>
2021-03-21 17:39:01 +0100

    styling: nesting styles on hover

--- src/components/buttons/PurchaseButton.js
+++ src/components/buttons/PurchaseButton.js
@@ -8,7 +8,7 @@ export default function PurchaseButton({ title, subtitle }) {
     <Link to="/page-2">
       <Wrapper>
         <IconWrapper>
-          <Icon src="/images/icons/credit.svg" />
+          <Icon src="/images/icons/credit.svg" className="icon" />
           <Ring src="/images/icons/icon-ring.svg" />
         </IconWrapper>
         <TextWrapper>
@@ -32,6 +32,19 @@ const Wrapper = styled.div`
   grid-template-columns: 53px auto;
   align-items: center;
   gap: 20px;
+  *,
+  & {
+    transition: 1s cubic-bezier(0.075, 0.82, 0.165, 1);
+  }
+  :hover {
+    box-shadow: 0px 1px 3px rgba(0, 0, 0, 0.1),
+      0px 30px 60px rgba(23, 0, 102, 0.5),
+      inset 0px 0px 0px 0.5px rgba(255, 255, 255, 0.5);
+    transform: translateY(-3px);
+    .icon {
+      transform: scale(1.2);
+    }
+  }
 `
 const Title = styled(Caption2)`
   color: black;
@@ -55,6 +68,9 @@ const IconWrapper = styled.div`
   align-content: center;
   justify-self: center;
   position: relative;
+  ${Wrapper}:hover & {
+    filter: hue-rotate(10deg) brightness(150%) saturate(120%);
+  }
 `
 const TextWrapper = styled.div`
   display: grid;
@@ -64,4 +80,8 @@ const Ring = styled.img`
   position: absolute;
   top: -15px;
   left: -16px;
+
+  ${Wrapper}:hover & {
+    transform: rotate(30deg) scale(1.2) translate(1px, 1px);
+  }
 `
